eb120aa87e3f4d54c567cf8952a7c03aa5254d9d
jjooyoun <jjooyoun@gmail.com>
2015-12-31 19:35:18 -0800

    UI android
    UI android Init

---
+++ app/src/main/java/com/ui/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.ui.com.ui_android.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
